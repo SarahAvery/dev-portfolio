5b3d450ddbbca669980bf5a5ecd6bda44cb2230d
S Avery <sarahashavery@gmail.com>
2022-07-30 14:34:32 -0400

    Updates

--- js/index.js
+++ js/index.js
@@ -1,18 +1,17 @@
-(function () {
-  if (window.localStorage) {
-    // create 'reload'
-    if (!localStorage.getItem("reload")) {
-      localStorage["reload"] = true;
-      window.location.reload();
-      console.log("RELOADED");
-    } else {
-      // remove 'reload'
-      localStorage.removeItem("reload");
-    }
-  }
-})(); // Call & init */
-
-
+// (() => {
+//   if (window.localStorage) {
+//     // create 'reload'
+//     if (!localStorage.getItem("reload")) {
+//       localStorage["reload"] = true;
+//       window.location.reload();
+//       console.log("RELOADED");
+//     } else {
+//       // remove 'reload'
+//       localStorage.removeItem("reload");
+//     }
+//   }
+// })();
+// Call & init */
 $(document).ready(function () {
   $(".ba-slider").each(function () {
     var cur = $(this); // Adjust the slider
