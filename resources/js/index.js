88b81df7db88dcfdd9959355f16c4ed3196590cd
S Avery <sarahashavery@gmail.com>
2022-07-30 14:31:46 -0400

    add some carz to portfolio

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,16 +1,16 @@
-(() => {
-  if (window.localStorage) {
-    // create 'reload'
-    if (!localStorage.getItem("reload")) {
-      localStorage["reload"] = true;
-      window.location.reload();
-      console.log("RELOADED");
-    } else {
-      // remove 'reload'
-      localStorage.removeItem("reload");
-    }
-  }
-})();
+// (() => {
+//   if (window.localStorage) {
+//     // create 'reload'
+//     if (!localStorage.getItem("reload")) {
+//       localStorage["reload"] = true;
+//       window.location.reload();
+//       console.log("RELOADED");
+//     } else {
+//       // remove 'reload'
+//       localStorage.removeItem("reload");
+//     }
+//   }
+// })();
 
 // Call & init */
 $(document).ready(function () {
